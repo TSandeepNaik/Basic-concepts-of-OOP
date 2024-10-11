3770d3d022461da1b1b10fe6ae3391802f171e0e
TSandeepNaik <sandeepnaikat100@gmail.com>
2024-10-11 14:29:47 +0530

    deleted polymorphism is added back

---
+++ src/Polymorphism/PolymorphismExample.java
@@ -0,0 +1,9 @@
+package Polymorphism;
+
+public class PolymorphismExample {
+    public static void main(String[] args) {
+        Sampleclass s1 = new Sampleclass();
+        s1.sum(3,5);
+        s1.sum(3, 2, 5);
+    }
+}
